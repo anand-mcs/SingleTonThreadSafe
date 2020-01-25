0a69c31a5ad303d58fa6a7da28032d8cd49dc8b4
anand-mcs <59893145+anand-mcs@users.noreply.github.com>
2020-01-25 18:00:11 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+//Anand Jayaraman
+namespace SingleTonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                ()=>ShoppingCart1(),
+                ()=>ShoppingCart2());
+
+            Console.WriteLine("Time To Terminate?");
+        }
+
+        private static void ShoppingCart2()
+        {
+            SingleTon obj2 = SingleTon.GetInstance;
+            obj2.SingleTonValue = 2;
+            obj2.Display("From Shopping Cart 2 " + "Shared Property value " + obj2.SingleTonValue.ToString());
+        }
+
+        private static void ShoppingCart1()
+        {
+            SingleTon obj1 = SingleTon.GetInstance;
+            obj1.SingleTonValue = 1;
+            obj1.Display("From Shopping Cart 1 " + "Shared Property value " + obj1.SingleTonValue.ToString());
+        }
+    }
+}
---
+++ SingleTon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Anand Jayaraman
+namespace SingleTonPattern
+{
+    public sealed class SingleTon
+    {
+        private static int counter;
+        private static readonly object obj = new object();
+        //Changed the constructor to private to control the instantiation.
+        private static SingleTon Instance = null;
+        public int SingleTonValue = 0;
+
+        public static SingleTon GetInstance
+        {
+            get
+            {
+                //Double check locking with instance check null before lock and after.
+                if (Instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (Instance == null)
+                            Instance = new SingleTon();
+                    }
+                }
+                return Instance;
+
+            }
+        }
+       
+        
+    private SingleTon()
+        {
+            counter++;
+            SingleTonValue = 0;
+            Console.WriteLine("Shopping Cart Single Instance kicked off " + counter.ToString());
+    
+        }
+        public void Display(string Msg)
+        {
+            Console.WriteLine(Msg);
+        }
+    }
+}
